bb48cea02f2c2bd2486a5a4de31266d3ea2c63d2
raj-dhillon <97066051+raj-dhillon@users.noreply.github.com>
2022-11-16 21:08:53 -0800

    adjusted text spacing

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,14 +8,14 @@ const About = () => {
             <div>Raj</div> 
             
         </div>
-        <div className='flex flex-wrap mx-14 mt-20 justify-center text-center font-nunito font-bold text-lg'>
+        <div className='flex flex-wrap mx-14 mt-20 justify-center text-center font-nunito font-bold text-lg lg:mx-36'>
             
             <div>
-                I'm a Computer Science student at SPU, graduating in spring 2024. Among my hobbies are gaming, reading, exercising, and enjoying science fiction/fantasy films
+                I'm a Computer Science student at SPU, graduating in spring 2024. Among my hobbies are gaming, reading, exercising, and enjoying science fiction/fantasy films.
             </div>
 
             <div className='mt-8'>
-                I understand that improvement requires you to leave your comfort zone, so I strive to undertake things that I know will cause me discomfort in the name of growth
+                I understand that improvement requires you to leave your comfort zone, so I strive to undertake things that I know will cause me discomfort in the name of growth.
             </div>
 
         </div>
