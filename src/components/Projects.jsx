5b536b6ae895d6dcd6b187dbeb5bc6bb5211304c
raj-dhillon <97066051+raj-dhillon@users.noreply.github.com>
2023-04-09 23:03:36 -0700

    added dota/lol image classifier to projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,7 +11,7 @@ const Projects = () => {
     
     <div className='flex flex-col md:flex-row items-center justify-center'>
 
-      <div className='grid grid-cols-1 md:grid-cols-2 gap-4 mb-8'>
+      <div className='grid grid-cols-1 md:grid-cols-3 gap-4 mb-8'>
         {projects.map(project => (
           <ProjectsItem 
             imgUrl={project.imgUrl}
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,15 +1,21 @@
 export default [
     {
-        title: 'News API CLI',
-        imgUrl: 'assets/news_cli.png',
-        stack: ['Python'],
-        link: 'https://github.com/raj-dhillon/news-cli',
+        title: 'Dota or LoL Image Classifier',
+        imgUrl: 'assets/dota_lol_classifier.png',
+        stack: ['fastai', 'Gradio'],
+        link: 'https://huggingface.co/spaces/raj-dhillon/dota_lol',
     },
     {
         title: 'Portfolio Site',
         imgUrl: 'assets/portfolio_site.png',
         stack: ['React', 'Tailwind'],
         link: 'https://rajsdhillon.com',
+    },
+    {
+        title: 'News API CLI',
+        imgUrl: 'assets/news_cli.png',
+        stack: ['Python'],
+        link: 'https://github.com/raj-dhillon/news-cli',
     },
 
 ]
