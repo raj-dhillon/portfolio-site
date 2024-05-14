0997c36a6fd5c8d79c37b5ace46de81b278f0e4c
raj-dhillon <97066051+raj-dhillon@users.noreply.github.com>
2024-05-13 20:57:04 -0700

    updated project info

--- src/components/ProjectsItem.jsx
+++ src/components/ProjectsItem.jsx
@@ -6,9 +6,9 @@ function ProjectsItem({title, imgUrl, stack, link, desc}) {
             <img src={imgUrl} alt="projects" className='w-full h-36 md:h-48 object-cover cursor-pointer'/>
 
             <div className='w-full p-4'>
-                <h3 className='text-lg md:text-xl mb-2 md:mb-3 font-semibold'>{title}</h3>
+                <h3 className='text-lg md:text-xl mb-2 md:mb-3 font-semibold'>{title}</h3>            
                 <div className='mb-2 md:mb-3 overflow-y-auto h-20 md:h-24'>
-                    <h4>{desc}</h4>
+                    <p>{desc}</p>
                 </div>
                 
                 <p className='flex flex-wrap gap-2 items-center justify-start text-xs md:text-sm'>
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,17 +1,31 @@
 const projects = [
     {
+        title: 'Intention (Personal CRM)',
+        imgUrl: 'assets/intention_app.png',
+        stack: ['React Native', 'Expo', 'Flask', 'MySQL', 'WhisperAI', 'GPT', 'Clerk', 'Railway'],
+        link: 'assets/intention_erickson.pptx',
+        desc: 'This is a five person team project for my Senior Capstone. ' +
+                'Intention uses the user\'s notes about their contacts along with ' +
+                'leveraging LLMs to provide them with conversation-starting ' + 
+                'questions- allowing them to connect with those who are important to them effortlessly.'
+    },
+    {
         title: 'Major Schedule Generator',
         imgUrl: 'assets/major_schedule_generator.png',
         stack: ['Python', 'NetworkX'],
         link: 'https://github.com/raj-dhillon/major-schedule-generator',
-        desc: 'This was team project for a course.',
+        desc: 'This was a two person team project for my Algorithm Design course. ' +
+                'We took in a csv file containing the major requirements for a student, ' +
+                'and returned a schedule they could follow to graduate on time.'
     },
     {
         title: 'Dota or LoL Image Classifier',
         imgUrl: 'assets/dota_lol_classifier.png',
         stack: ['fastai', 'Gradio'],
         link: 'https://huggingface.co/spaces/raj-dhillon/dota_lol',
-        desc: 'This was a solo project that I undertook to better understand fastai.',
+        desc: 'This was a solo project that I undertook to better understand fastai. ' +
+                'It serves to showcase how rapidly you can fine-tune a functional model ' +
+                'using conservative hardware, such as my M1 MacBook Air.'
     },
     {
         title: 'Portfolio Site',
