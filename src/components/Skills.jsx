cc70361f345f6d53bf79b73ab44c5709d6d97c1e
raj-dhillon <97066051+raj-dhillon@users.noreply.github.com>
2025-09-09 22:29:58 -0700

    added new skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -11,6 +11,14 @@ import {ReactComponent as Expo} from '../rsc/expo.svg'
 import {ReactComponent as MySQL} from '../rsc/mysql.svg'
 import {ReactComponent as AWS} from '../rsc/aws.svg'
 import {ReactComponent as OpenAI} from '../rsc/openai.svg'
+import {ReactComponent as JavaScriptLogo} from '../rsc/javascript.svg'
+import {ReactComponent as KubernetesLogo} from '../rsc/kubernetes.svg'
+import {ReactComponent as ExpressLogo} from '../rsc/express.svg'
+import {ReactComponent as FlaskLogo} from '../rsc/flask.svg'
+import {ReactComponent as PostmanLogo} from '../rsc/postman.svg'
+import {ReactComponent as LinuxLogo} from '../rsc/linux.svg'
+import {ReactComponent as TypeScriptLogo} from '../rsc/typescript.svg'
+import {ReactComponent as JenkinsLogo} from '../rsc/jenkins.svg'
 
 const Skills = () => {
   return (
@@ -21,18 +29,36 @@ const Skills = () => {
             </div>
     
             <div className='mt-16 grid grid-cols-4 gap-5 md:grid-cols-6 md:mx-14'>
-              <Expo className='w-20'></Expo>
-              <MySQL className='w-20'></MySQL>
-              <AWS className='w-20'></AWS>
+              {/* Languages & Databases */}
               <PythonLogo className='w-20'></PythonLogo>
-              <OpenAI className='w-20'></OpenAI>
-              <GithubLogo className='w-20'></GithubLogo>
-              <ReactLogo className='w-20'></ReactLogo>
+              <TypeScriptLogo className='w-20'></TypeScriptLogo>
               <CPPLogo className='w-20'></CPPLogo>
-              <TailwindLogo className='w-20'></TailwindLogo>
+              <JavaScriptLogo className='w-20'></JavaScriptLogo>
+              <MySQL className='w-20'></MySQL>
               <SQLiteLogo className='w-20'></SQLiteLogo>
-              <DockerLogo className='w-20'></DockerLogo>
+
+              {/* Frontend */}
+              <ReactLogo className='w-20'></ReactLogo>
+              <Expo className='w-20'></Expo>
+              <TailwindLogo className='w-20'></TailwindLogo>
               <QtLogo className='w-20'></QtLogo>
+
+              {/* Backend */}
+              <ExpressLogo className='w-20'></ExpressLogo>
+              <FlaskLogo className='w-20'></FlaskLogo>
+
+              {/* Cloud & DevOps */}
+              <AWS className='w-20'></AWS>
+              <DockerLogo className='w-20'></DockerLogo>
+              <KubernetesLogo className='w-20'></KubernetesLogo>
+              <GithubLogo className='w-20'></GithubLogo>
+              <JenkinsLogo className='w-20'></JenkinsLogo>
+              <LinuxLogo className='w-20'></LinuxLogo>
+
+              {/* Tools & AI */}
+              <PostmanLogo className='w-20'></PostmanLogo>
+              <OpenAI className='w-20'></OpenAI>
+
 
             </div>
 
